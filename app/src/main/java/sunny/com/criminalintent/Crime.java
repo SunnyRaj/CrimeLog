5a86d6cb56046098754919199d8acfeef66db9bd
Sunny Raj <sundarcs@gwu.edu>
2016-05-28 15:21:40 -0400

    Wire up all widgets

--- app/src/main/java/sunny/com/criminalintent/Crime.java
+++ app/src/main/java/sunny/com/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package sunny.com.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,12 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public void crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
     public UUID getId(){
         return mId;
@@ -23,4 +27,14 @@ public class Crime {
         return mTitle;
     }
 
+    public Date getDate(){
+        return mDate;
+    }
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
 }
--- app/src/main/java/sunny/com/criminalintent/CriminalActivity.java
+++ app/src/main/java/sunny/com/criminalintent/CriminalActivity.java
@@ -1,13 +1,25 @@
 package sunny.com.criminalintent;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-public class CriminalActivity extends AppCompatActivity {
+public class CriminalActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criminal);
+
+        FragmentManager fm =
+        CrimeFragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
+
     }
 }
